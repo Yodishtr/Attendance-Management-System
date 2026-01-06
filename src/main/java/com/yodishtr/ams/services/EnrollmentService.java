bc288800da913a0448794a5dc5c704f27eca01c8
vythili6 <yo.vythilingum@mail.utoronto.ca>
2026-01-06 01:05:30 -0500

    implementing the Enrollment Service

---
+++ src/main/java/com/yodishtr/ams/services/EnrollmentService.java
@@ -0,0 +1,15 @@
+package com.yodishtr.ams.services;
+
+import com.yodishtr.ams.repositories.EnrollmentRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EnrollmentService {
+    private final EnrollmentRepository enrollmentRepository;
+
+    public EnrollmentService(EnrollmentRepository enrollmentRepository){
+        this.enrollmentRepository = enrollmentRepository;
+    }
+
+
+}
